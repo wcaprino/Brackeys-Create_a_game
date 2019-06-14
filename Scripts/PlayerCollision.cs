89f737e215af0fd21c160851835e24ca28100bba
wcaprino <51685088+wcaprino@users.noreply.github.com>
2019-06-13 23:37:34 -0300

    Add files via upload

---
+++ Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    
+
+    void OnCollisionEnter( Collision collisionInfo)
+      {
+
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+        // Ao bater em obstáculo, cessa movimento
+           movement.enabled = false;    
+        }
+       // Debug.Log(collisionInfo.collider.tag);
+    }
+}
