7e32c0e2c805f807c47a2502f20b70be1d4958f2
wcaprino <wcaprino@gmail.com>
2019-06-24 23:48:59 -0300

    Na verdade 8
    Agora vai

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    
+
+    void OnCollisionEnter( Collision collisionInfo)
+      {
+
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+        // Ao bater em obstáculo, cessa movimento
+           movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+       // Debug.Log(collisionInfo.collider.tag);
+    }
+}
